69440f1b8973beb907504cdf693b2ca5ea829a58
Dmitry Mozzherin <dmozzherin@gmail.com>
2023-06-20 19:55:34 +0000

    update world of vascular plants outlink

--- rebuild/file_data_sources.go
+++ rebuild/file_data_sources.go
@@ -392,7 +392,7 @@ var DataSourcesInf = map[int]DataSourceInf{
 		UUID:           "814d1a77-2234-449b-af4a-138e0e1b1326",
 		HomeURL:        "https://wcvp.science.kew.org/",
 		IsOutlinkReady: true,
-		OutlinkURL:     "https://wcvp.science.kew.org/taxon/{}",
+		OutlinkURL:     "https://powo.science.kew.org/taxon/urn:lsid:ipni.org:names:{}",
 		OutlinkID: func(n NameInf) string {
 			return n.RecordID
 		},
